20f57ceda3990f5073397f5ef5775fb6e157283e
Ramadhan Mkoma <Ramadhanmkoma9@gmail.com>
2023-07-14 11:03:31 +0300

    ultra initial

---
+++ src/test/java/io/techswahili/biometric/BiometricApplicationTests.java
@@ -0,0 +1,13 @@
+package io.techswahili.biometric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
